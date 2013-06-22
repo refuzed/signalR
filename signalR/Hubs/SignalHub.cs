e6ff1ec5215f1e881c5bb4de9e70115fce682c8a
sean oneil <refuzed@gmail.com>
2013-06-22 02:15:38 -0500

    Added some more fancy things
    BUTTONS

--- signalR/Hubs/SignalHub.cs
+++ signalR/Hubs/SignalHub.cs
@@ -8,21 +8,48 @@ namespace signalR.Hubs
 {
     public class SignalHub : Hub
     {
+        private static int tickspeed;
         private static int counter;
         private static bool ticking;
 
         public void StartTicker()
         {
+            if (tickspeed == 0) tickspeed = 1000;
+
             if (!ticking)
             {
                 ticking = true;
-                Task.Run(() => { while (ticking) { Clients.All.addNewMessageToPage(new { name = "Tick", message = counter }); counter++; Thread.Sleep(1000); } });
+                Task.Run(() =>
+                {
+                    while (ticking)
+                    {
+                        counter++; 
+                        Clients.All.addNewMessageToPage(new { name = "Tick", message = counter, tickspeed = tickspeed }); 
+                        Thread.Sleep(tickspeed);
+                    }
+                });
             }
         }
 
         public void StopTicker()
         {
             ticking = false;
+        }
+
+        public void ResetTicker()
+        {
+            counter = 0;
+        }
+
+        public void SpeedUp()
+        {
+            if (tickspeed > 100)
+                tickspeed -= 100;
+        }
+
+        public void SlowDown()
+        {
+            tickspeed += 100;
         }
     }
 }
