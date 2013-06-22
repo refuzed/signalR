28c0e59a7a9650fc1825d4358a7df78cf4d12642
sean oneil <refuzed@gmail.com>
2013-06-22 01:47:31 -0500

    Added Timer
    Fancypants

--- signalR/Controllers/HomeController.cs
+++ signalR/Controllers/HomeController.cs
@@ -10,23 +10,8 @@ namespace signalR.Controllers
 {
     public class HomeController : Controller
     {
-        private static readonly IHubContext _hubContext = GlobalHost.ConnectionManager.GetHubContext<SignalHub>();
-        public static int counter;
-
         public ActionResult Index()
         {
-            return RedirectToAction("Updater");
-        }
-
-        public ActionResult Updater()
-        {
-            return View();
-        }
-
-        public ActionResult HitMe()
-        {
-            counter++;
-            _hubContext.Clients.All.addNewMessageToPage(new { name = "name", message=counter });
             return View();
         }
     }
--- signalR/Hubs/SignalHub.cs
+++ signalR/Hubs/SignalHub.cs
@@ -1,10 +1,28 @@
 ﻿using System;
 using System.Web;
+using System.Threading;
+using System.Threading.Tasks;
 using Microsoft.AspNet.SignalR;
 
 namespace signalR.Hubs
 {
     public class SignalHub : Hub
     {
+        private static int counter;
+        private static bool ticking;
+
+        public void StartTicker()
+        {
+            if (!ticking)
+            {
+                ticking = true;
+                Task.Run(() => { while (ticking) { Clients.All.addNewMessageToPage(new { name = "Tick", message = counter }); counter++; Thread.Sleep(1000); } });
+            }
+        }
+
+        public void StopTicker()
+        {
+            ticking = false;
+        }
     }
 }
