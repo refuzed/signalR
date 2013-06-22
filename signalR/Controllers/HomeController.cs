0fe4806b2d7341697261ddcf8b726dc3566b5c55
sean oneil <refuzed@gmail.com>
2013-06-22 01:21:21 -0500

    Initial Test Pages
    It works.

---
+++ signalR/Controllers/HomeController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Microsoft.AspNet.SignalR;
+using signalR.Hubs;
+
+namespace signalR.Controllers
+{
+    public class HomeController : Controller
+    {
+        private static readonly IHubContext _hubContext = GlobalHost.ConnectionManager.GetHubContext<SignalHub>();
+        public static int counter;
+
+        public ActionResult Index()
+        {
+            return RedirectToAction("Updater");
+        }
+
+        public ActionResult Updater()
+        {
+            return View();
+        }
+
+        public ActionResult HitMe()
+        {
+            counter++;
+            _hubContext.Clients.All.addNewMessageToPage(new { name = "name", message=counter });
+            return View();
+        }
+    }
+}
---
+++ signalR/Hubs/SignalHub.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Web;
+using Microsoft.AspNet.SignalR;
+
+namespace signalR.Hubs
+{
+    public class SignalHub : Hub
+    {
+    }
+}
